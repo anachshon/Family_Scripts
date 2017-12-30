e34b1a0e3b57ea331f140fb50337b203884e32ce
anachshon <Hub2014>
2017-12-30 22:35:29 +0200

    A basic script to take a google locations file and convert it to csv.

---
+++ Yaniv_Dec_2017/Google_Locations-1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os, sys, shutil
+import json
+import datetime
+import gzip
+
+inp_file_name = sys.argv[ 1 ]
+out_file_name = sys.argv[ 2 ]
+
+#{u'latitudeE7': 321315952, u'altitude': 63, u'longitudeE7': 349514971, u'velocity': 4, u'timestampMs': u'1474264307000', u'heading': 218, u'accuracy': 13}
+
+gkeys = { 'locs' : 'locations', 'long' : 'longitudeE7', 'lat' : 'latitudeE7', 'time' : 'timestampMs' }
+
+inp_file = gzip.open( inp_file_name, 'r' )
+data = json.load( inp_file )
+inp_file.close()
+
+out_file = gzip.open( out_file_name, 'w' )
+out_file.write( '\time\tlong\tlat\n' )
+for entry in data[ gkeys[ 'locs' ] ]:
+    long = float( entry[ gkeys[ 'long' ] ] ) / 1e7
+    lat = float( entry[ gkeys[ 'lat' ] ] ) / 1e7
+    time = datetime.datetime.fromtimestamp( float( entry[ gkeys[ 'time' ] ] ) / 1e3 )
+
+    out_file.write( time.strftime( "%a, %d %b %Y %H:%M:%S") + '\t' + str( long ) + '\t' + str( lat ) + '\n' )
+
+out_file.close()
+
