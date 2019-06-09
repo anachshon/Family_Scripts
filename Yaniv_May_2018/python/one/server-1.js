c0a87c42eeec234bb1777f31987f61ba9287001f
Aharon Nachshon <anachshon@gmail.com>
2019-06-09 07:38:54 +0300

    This looks like a kind of working version of the html / js side. On clicking the button on the html GUI, a TCP message is sent.

--- Yaniv_May_2018/python/one/server-1.js
+++ Yaniv_May_2018/python/one/server-1.js
@@ -2,12 +2,23 @@
 const http = require( 'http' );
 const url = require( 'url' );
 const fs = require( 'fs' );
+const net = require('net');
 
 http.createServer((request, response) => {
 
     //console.log( request )
     var queryData = url.parse(request.url, true).query;
-    console.log( queryData );
+    cmd = queryData.command;
+    if ( cmd == 'sendmail' )
+        arg = queryData.mail
+    else
+        arg = ''
+    if ( cmd != null && cmd != '' )
+    {
+        send_tcp( cmd, arg );
+        console.log( 'Command : ' + queryData.command );
+    }
+//        console.log( queryData.mail );
 
   fs.readFile('ex1.html', function(err, data) {
     response.writeHead(200, {'Content-Type': 'text/html'});
@@ -15,3 +26,19 @@ http.createServer((request, response) => {
     response.end();
 })
 }).listen(1337);
+
+function send_tcp( cmd, arg )
+{
+    var client = new net.Socket();
+    client.connect( 10000, '127.0.0.1', function() {
+        console.log('Connected');
+        if ( arg != '' )
+            client.write( cmd + '&' + arg );
+        else
+            client.write( cmd );
+    });
+    client.on('data', function(data) {
+	    console.log('Received: ' + data);
+	    client.destroy();
+    });
+}
