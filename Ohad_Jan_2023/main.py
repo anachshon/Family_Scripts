5fc9803eb9c3bdea625083d2e8cc3f88e071127d
anachshon <anachshon@gmail.com>
2023-01-28 09:29:47 +0200

    Moving to the same color functions where nothing in defined (colors wise) in advance and the pattern should be consistent in all the faces. For now just the 1d solution, and also this one is probably not complete. It is always start with the first cube which should not be the case.

--- Ohad_Jan_2023/main.py
+++ Ohad_Jan_2023/main.py
@@ -2,6 +2,7 @@
 
 import cube
 import solve
+import same_color_solve_1d
 import dics
 import pandas as pd
 import sys
@@ -12,7 +13,10 @@ cubes = []
 for line in range( data.shape[ 0 ] ):
     cubes.append( cube.cube( line + 1, data.iloc[ line, range( 6 ) ], data.iloc[ line, range( 6, 12 ) ] ) )
 
-if ( len( sys.argv ) == 4 ):
-    solve.solve_1d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ) )
-elif ( len( sys.argv ) == 5 ):
-    solve.solve_2d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ), int( sys.argv[ 4 ] ) )
+#if ( len( sys.argv ) == 4 ):
+#    solve.solve_1d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ) )
+#elif ( len( sys.argv ) == 5 ):
+#    solve.solve_2d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ), int( sys.argv[ 4 ] ) )
+
+if ( len( sys.argv ) == 3 ):
+    same_color_solve_1d.solve_1d( cubes, int( sys.argv[ 2 ] ) )
---
+++ Ohad_Jan_2023/same_color_solve_1d.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import cube
+import dics
+import copy
+
+#
+#   1D
+#
+
+def solve_1d( cubes, length ):
+
+    nof_cubes = len( cubes )
+    nof_soln = 0
+
+    colors = 6 * [ -1 ]
+
+    def write_soln( solution ):
+
+        nonlocal nof_soln
+
+        nof_soln += 1
+        line = [ nof_soln, ' : ' ]
+        for n in range( len( solution ) ):
+            line.append( '(' + str( n + 1 ) + ')' )
+            cur_cube = solution[ n ]
+            line += [
+                       cur_cube.get_index(),
+                       dics.colors_inv[ cur_cube.cols[ cur_cube.get_y() - 1 ] ],
+                       dics.colors_inv[ cur_cube.cols[ cur_cube.get_x() - 1 ] ]
+                    ]
+        print( '\t'.join( [ str( x ) for x in line ] ) )
+
+    def aux_solve( solution, used ):
+
+        nonlocal colors
+
+        for n in range( nof_cubes ):
+            if ( not used[ n ] ):
+                if ( len( solution ) == 0 ):
+                    for y in range( 1, 7 ):
+                        for x in dics.valid_vals[ y ]:
+                            cur_cube = copy.deepcopy( cubes[ n ] )
+                            cur_cube.set_y( y )
+                            cur_cube.set_x( x )
+                            colors[ dics.f.up ] = cur_cube.get_up()[ 0 ]
+                            colors[ dics.f.down ] = cur_cube.get_down()[ 0 ]
+                            colors[ dics.f.back ] = cur_cube.get_back()[ 0 ]
+                            colors[ dics.f.front ] = cur_cube.get_front()[ 0 ]
+                            new_solution = copy.deepcopy( solution )
+                            new_used = copy.deepcopy( used )
+                            new_solution.append( cur_cube )
+                            new_used[ n ] = True
+                            if ( len( new_solution ) == length ):
+                                write_soln( new_solution )
+                            else:
+                                if ( n < nof_cubes - 1 ):
+                                    aux_solve( new_solution, new_used )
+                else:
+                    cur_cube = copy.deepcopy( cubes[ n ] )
+                    y = cur_cube.has_color( colors[ dics.f.up ] )
+                    if ( not y ):
+                        continue
+                    else:
+                        cur_cube.set_y( y )
+                        found = False
+                        for x in dics.valid_vals[ y ]:
+                            cur_cube.set_x( x )
+                            if (
+                                    cur_cube.get_down()[ 0 ] == colors[ dics.f.down ] and
+                                    cur_cube.get_front()[ 0 ] == colors[ dics.f.front ] and
+                                    cur_cube.get_back()[ 0 ] == colors[ dics.f.back ]
+                                ):
+                                found = True
+                                break
+                        if ( not found ):
+                            continue
+                        else:
+                            prev_cube = solution[ -1 ]
+                            if ( not prev_cube.match_right( cur_cube ) ):
+                                continue
+                            else:
+                                new_solution = copy.deepcopy( solution )
+                                new_used = copy.deepcopy( used )
+                                new_solution.append( cur_cube )
+                                new_used[ n ] = True
+                                if ( len( new_solution ) == length ):
+                                    write_soln( new_solution )
+                                else:
+                                    if ( n < nof_cubes - 1 ):
+                                        aux_solve( new_solution, new_used )
+
+    aux_solve( [], nof_cubes * [ False ] )
