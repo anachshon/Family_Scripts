1daa74940f5dd9fcfb008da987536bc50e251455
anachshon <anachshon@gmail.com>
2023-03-31 22:58:45 +0300

    Organizing simpler / more explicit the input arguments.

--- Ohad_Jan_2023/main.py
+++ Ohad_Jan_2023/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import os
 
 import cube
 import solve
 import same_color_solve_1d
 import same_color_solve_2d
+import solve_general
 import dics
 import pandas as pd
 import sys
+import runpy
 
 data = pd.read_csv( sys.argv[ 1 ], sep = "\t", index_col = 0, header = 0 )
 cubes = []
@@ -14,13 +17,13 @@ cubes = []
 for line in range( data.shape[ 0 ] ):
     cubes.append( cube.cube( line + 1, data.iloc[ line, range( 6 ) ], data.iloc[ line, range( 6, 12 ) ] ) )
 
-#if ( len( sys.argv ) == 4 ):
-#    solve.solve_1d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ) )
-#elif ( len( sys.argv ) == 5 ):
-#    solve.solve_2d( cubes, dics.colors[ sys.argv[ 2 ] ], int( sys.argv[ 3 ] ), int( sys.argv[ 4 ] ) )
-
-if ( len( sys.argv ) == 3 ):
-    same_color_solve_1d.solve_1d( cubes, int( sys.argv[ 2 ] ) )
-
-if ( len( sys.argv ) == 4 ):
-    same_color_solve_2d.solve_2d( cubes, int( sys.argv[ 2 ] ), int( sys.argv[ 3 ] ) )
+match sys.argv[ 2 ]:
+    case '1d':
+        same_color_solve_1d.solve_1d( cubes, int(sys.argv[ 3 ] ) )
+    case '2d':
+        same_color_solve_2d.solve_2d( cubes, int( sys.argv[ 3 ] ), int( sys.argv[ 4 ] ) )
+    case 'gen':
+        solve_general.solve( cubes, sys.argv[ 3 ] )
+    case other:
+        print( 'The second argument should be either 1d, 2d, or gen' )
+        exit()
