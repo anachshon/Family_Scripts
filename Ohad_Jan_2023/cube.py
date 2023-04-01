ac0d8286cb260d328aa6311b67862d2214b2c746
anachshon <anachshon@gmail.com>
2023-04-01 14:05:25 +0300

    Adding function which are necessary to the solve general function. It looks that many addiitonal functions can be written in a more concise way.

--- Ohad_Jan_2023/cube.py
+++ Ohad_Jan_2023/cube.py
@@ -81,8 +81,72 @@ class cube:
         mag_other = other.get_up()[ 1 ]
         return( mag_me != mag_other )
 
+    def match_left( self, other ):
+        mag_me = self.get_left()[ 1 ]
+        mag_other = other.get_right()[ 1 ]
+        return( mag_me != mag_other )
+
+    def match_back( self, other ):
+        mag_me = self.get_back()[ 1 ]
+        mag_other = other.get_front()[ 1 ]
+        return( mag_me != mag_other )
+
+    def match_up( self, other ):
+        mag_me = self.get_up()[ 1 ]
+        mag_other = other.get_down()[ 1 ]
+        return( mag_me != mag_other )
+
     def has_color( self, color ):
         if ( color in self.cols ):
             return( self.cols.index( color ) + 1 )
         else:
             return( 0 )
+
+#    def get( self, side ):
+#
+#        match side:
+#            case "Right":
+#                return( self.get_right() )
+#            case "Left":
+#                return( self.get_left() )
+#            case "Front":
+#                return( self.get_front() )
+#            case "Back":
+#                return( self.get_back() )
+#            case "Top":
+#                return( self.get_up() )
+#            case "Down":
+#                return( self.get_down() )
+
+    def get( self, side ):
+
+        match side:
+            case dics.f.right:
+                return( self.get_right() )
+            case dics.f.left:
+                return( self.get_left() )
+            case dics.f.front:
+                return( self.get_front() )
+            case dics.f.back:
+                return( self.get_back() )
+            case dics.f.up:
+                return( self.get_up() )
+            case dics.f.down:
+                return( self.get_down() )
+
+
+    def match_mag( self, side, other ):
+
+        match side:
+            case dics.f.right:
+                return( self.match_right( other ) )
+            case dics.f.left:
+                return( self.match_left( other ) )
+            case dics.f.front:
+                return( self.match_front( other ) )
+            case dics.f.back:
+                return( self.match_back( other ) )
+            case dics.f.up:
+                return( self.match_up( other ) )
+            case dics.f.down:
+                return( self.match_down( other ) )
